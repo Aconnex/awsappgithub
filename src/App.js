7726c7cc18e3b5412e05c7a8e65324b6d47c4f4d
Bartholdy Alex <you@example.com>
2023-03-20 23:35:47 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      hello world... monday... 11:33... Alex
+    </React.Fragment>
+  );
+}
+
+export default App;
